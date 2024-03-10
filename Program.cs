b71b4f68e79c9e6dfb1309562a442c0fa6bdf733
Alexander <bl055@rambler.ru>
2024-03-11 00:16:00 +0300

    save1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string OutputNumbers(int m, int n)
+{
+    if (n == m)
+        return $"{m}";
+    return OutputNumbers(m, n - 1) + $", {n}";
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-e число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-e число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(OutputNumbers(m, n));
+
